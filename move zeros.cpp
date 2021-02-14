7b3f0d8b1bb0459e23e841036ccb10451bc6744c
akash <akash98211@gmail.com>
2021-02-14 13:01:06 +0530

    leet code day1 twowaits question

---
+++ move zeros.cpp
@@ -0,0 +1,21 @@
+public:
+    void moveZeroes(vector<int>& nums) {
+     int n=nums.size();
+        if(n==0 || n==1)
+            return;
+        
+        int left=0,right=0,temp;
+        
+        while(right<n)
+        {
+            if(nums[right]==0)
+                ++right;
+            else{
+            temp=nums[right];
+            nums[right]=nums[left];
+            nums[left]=temp;
+            ++left;right++;
+            }
+        }
+    }
+};
